cd1d9b9f96715ab1cc4da6b56d36a3706ffd556b
dankeroni <pana99@hotmail.de>
2016-06-05 16:29:14 +0200

    Revert "Created test for the Search function"
    This reverts commit 06cf25d48034775fd88c73d65cbe216b26c8b7a4.

--- main.go
+++ main.go
@@ -40,7 +40,10 @@ func main() {
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		results, err := Search(r.FormValue("search"))
+		var results []SearchResult
+		var err error
+
+		results, err = search(r.FormValue("search"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -52,18 +55,22 @@ func main() {
 	})
 
 	http.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
-		book, err := Find(r.FormValue("id"))
+		var book ClassifyBookResponse
+		var err error
+
+		book, err = find(r.FormValue("id"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-
 		err = db.Ping()
+
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 		_, err = db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
 			nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
+
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -76,22 +83,19 @@ type ClassifySearchResponse struct {
 	Results []SearchResult `xml:"works>work"`
 }
 
-type BookData struct {
-	Title  string `xml:"title,attr"`
-	Author string `xml:"author,attr"`
-	ID     string `xml:"owi,attr"`
-}
-
-type Classification struct {
-	MostPopular string `xml:"sfa,attr"`
-}
 type ClassifyBookResponse struct {
-	BookData       BookData       `xml:"work"`
-	Classification Classification `xml:"recommendations>dcc>mostPopular"`
+	BookData struct {
+		Title  string `xml:"title,attr"`
+		Author string `xml:"author,attr"`
+		ID     string `xml:"owi,attr"`
+	} `xml:"work"`
+	Classification struct {
+		MostPopular string `xml:"sfa,attr"`
+	} `xml:"recommendations>dcc>mostPopular"`
 }
 
-func Find(id string) (ClassifyBookResponse, error) {
-	body, err := classifyApi("http://classify.oclc.org/classify2/Classify?summary=true&owi=" + url.QueryEscape(id))
+func find(id string) (ClassifyBookResponse, error) {
+	body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?summary=true&owi=" + url.QueryEscape(id))
 	if err != nil {
 		return ClassifyBookResponse{}, err
 	}
@@ -101,10 +105,10 @@ func Find(id string) (ClassifyBookResponse, error) {
 	return c, err
 }
 
-func Search(query string) ([]SearchResult, error) {
+func search(query string) ([]SearchResult, error) {
 	query_url := "http://classify.oclc.org/classify2/Classify?summary=true&title=" + url.QueryEscape(query)
 
-	body, err := classifyApi(query_url)
+	body, err := classifyAPI(query_url)
 	if err != nil {
 		return []SearchResult{}, err
 	}
@@ -114,7 +118,7 @@ func Search(query string) ([]SearchResult, error) {
 	return c.Results, err
 }
 
-func classifyApi(req_url string) ([]byte, error) {
+func classifyAPI(req_url string) ([]byte, error) {
 	resp, err := http.Get(req_url)
 	if err != nil {
 		return []byte{}, err
