06cf25d48034775fd88c73d65cbe216b26c8b7a4
dankeroni <pana99@hotmail.de>
2016-06-05 16:14:56 +0200

    Created test for the Search function

--- main.go
+++ main.go
@@ -40,10 +40,7 @@ func main() {
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		var results []SearchResult
-		var err error
-
-		results, err = search(r.FormValue("search"))
+		results, err := Search(r.FormValue("search"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -55,22 +52,18 @@ func main() {
 	})
 
 	http.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
-		var book ClassifyBookResponse
-		var err error
-
-		book, err = find(r.FormValue("id"))
+		book, err := Find(r.FormValue("id"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		err = db.Ping()
 
+		err = db.Ping()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 		_, err = db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
 			nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
-
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -83,19 +76,22 @@ type ClassifySearchResponse struct {
 	Results []SearchResult `xml:"works>work"`
 }
 
-type ClassifyBookResponse struct {
-	BookData struct {
-		Title  string `xml:"title,attr"`
-		Author string `xml:"author,attr"`
-		ID     string `xml:"owi,attr"`
-	} `xml:"work"`
-	Classification struct {
-		MostPopular string `xml:"sfa,attr"`
-	} `xml:"recommendations>dcc>mostPopular"`
+type BookData struct {
+	Title  string `xml:"title,attr"`
+	Author string `xml:"author,attr"`
+	ID     string `xml:"owi,attr"`
 }
 
-func find(id string) (ClassifyBookResponse, error) {
-	body, err := classifyAPI("http://classify.oclc.org/classify2/Classify?summary=true&owi=" + url.QueryEscape(id))
+type Classification struct {
+	MostPopular string `xml:"sfa,attr"`
+}
+type ClassifyBookResponse struct {
+	BookData       BookData       `xml:"work"`
+	Classification Classification `xml:"recommendations>dcc>mostPopular"`
+}
+
+func Find(id string) (ClassifyBookResponse, error) {
+	body, err := classifyApi("http://classify.oclc.org/classify2/Classify?summary=true&owi=" + url.QueryEscape(id))
 	if err != nil {
 		return ClassifyBookResponse{}, err
 	}
@@ -105,10 +101,10 @@ func find(id string) (ClassifyBookResponse, error) {
 	return c, err
 }
 
-func search(query string) ([]SearchResult, error) {
+func Search(query string) ([]SearchResult, error) {
 	query_url := "http://classify.oclc.org/classify2/Classify?summary=true&title=" + url.QueryEscape(query)
 
-	body, err := classifyAPI(query_url)
+	body, err := classifyApi(query_url)
 	if err != nil {
 		return []SearchResult{}, err
 	}
@@ -118,7 +114,7 @@ func search(query string) ([]SearchResult, error) {
 	return c.Results, err
 }
 
-func classifyAPI(req_url string) ([]byte, error) {
+func classifyApi(req_url string) ([]byte, error) {
 	resp, err := http.Get(req_url)
 	if err != nil {
 		return []byte{}, err
---
+++ main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFind(t *testing.T) {
+	type FindTest struct {
+		id        string
+		expectedC ClassifyBookResponse
+		expectedR bool
+		expectedE bool
+	}
+
+	findTests := []FindTest{
+		FindTest{
+			id: "803736779",
+			expectedC: ClassifyBookResponse{
+				BookData: BookData{
+					Title:  "Cloning internet applications with Ruby : make your own TinyURL, Twitter, Flickr, or Facebook using Ruby",
+					Author: "Chang, Sau Sheong",
+					ID:     "803736779",
+				},
+				Classification: Classification{},
+			},
+			expectedR: true,
+			expectedE: false,
+		},
+	}
+
+	for _, test := range findTests {
+		c, err := Find(test.id)
+
+		if !test.expectedE {
+			assert.Nil(t, err)
+		} else {
+			assert.NotNil(t, err)
+		}
+
+		if test.expectedR {
+			assert.Equal(t, test.expectedC, c)
+		} else {
+			assert.NotEqual(t, test.expectedC, c)
+		}
+	}
+}
